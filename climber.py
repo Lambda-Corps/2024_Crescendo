49e89ff5fd7a32c33650055cc314d68b0679988b
Scott <scott@>
2024-04-02 16:51:01 -0400

    With limit switches

--- climber.py
+++ climber.py
@@ -1,35 +1,84 @@
 from commands2 import Subsystem, Command, cmd
-from phoenix5 import TalonSRX, TalonSRXControlMode
+from phoenix5 import (
+    TalonSRX,
+    TalonSRXControlMode,
+    TalonSRXConfiguration,
+    LimitSwitchSource,
+    Faults,
+    LimitSwitchNormal,
+)
 from wpilib import SmartDashboard, AnalogInput, RobotBase, Timer
 
 import constants
 
 
 class Climber(Subsystem):
+    CLIMBER_TOP_LIMIT = 5000
+    CLIMBER_UP_SPEED = 0.1
+    CLIMBER_DOWN_SPEED = 0.1
+
     def __init__(self) -> None:
         super().__init__()
 
         self._left_climber: TalonSRX = TalonSRX(constants.LEFT_CLIMBER)
         self._left_climber.configFactoryDefault()
+        self._left_climber.configReverseLimitSwitchSource(
+            LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0
+        )
+        self._left_climber.configForwardSoftLimitThreshold(self.CLIMBER_TOP_LIMIT)
+        self._left_climber.configForwardSoftLimitEnable(True)
 
         self._right_climber: TalonSRX = TalonSRX(constants.RIGHT_CLIMBER)
         self._right_climber.configFactoryDefault()
+        self._right_climber.configReverseLimitSwitchSource(
+            LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0
+        )
+        self._right_climber.configForwardSoftLimitThreshold(self.CLIMBER_TOP_LIMIT)
+        self._right_climber.configForwardSoftLimitEnable(True)
+
+        self._left_faults: Faults = Faults()
+        self._right_faults: Faults = Faults()
 
     def drive_climber_up(self) -> None:
-        self._left_climber.set(TalonSRXControlMode.PercentOutput, 1)
-        self._right_climber.set(TalonSRXControlMode.PercentOutput, 1)
+        self._left_climber.set(TalonSRXControlMode.PercentOutput, self.CLIMBER_UP_SPEED)
+        self._right_climber.set(
+            TalonSRXControlMode.PercentOutput, self.CLIMBER_UP_SPEED
+        )
 
     def drive_climber_down(self) -> None:
-        self._left_climber.set(TalonSRXControlMode.PercentOutput, -1)
-        self._right_climber.set(TalonSRXControlMode.PercentOutput, -1)
+        self._left_climber.set(
+            TalonSRXControlMode.PercentOutput, self.CLIMBER_DOWN_SPEED
+        )
+        self._right_climber.set(
+            TalonSRXControlMode.PercentOutput, self.CLIMBER_DOWN_SPEED
+        )
 
-    def climber_done(self) -> bool:
-        left_amps: float = self._left_climber.getStatorCurrent()
-        right_amps: float = self._right_climber.getStatorCurrent()
+    def climber_at_top(self) -> bool:
+        return (
+            self._left_faults.ForwardLimitSwitch
+            and self._right_faults.ForwardLimitSwitch
+        )
 
     def stop_climber_motors(self) -> None:
         self._left_climber.set(TalonSRXControlMode.PercentOutput, 0)
         self._right_climber.set(TalonSRXControlMode.PercentOutput, 0)
+
+    def periodic(self) -> None:
+        self._left_climber.getFaults(self._left_faults)
+        self._right_climber.getFaults(self._right_faults)
+
+        SmartDashboard.putBoolean(
+            "L Forward Limit", self._left_faults.ForwardLimitSwitch
+        )
+        SmartDashboard.putBoolean(
+            "R Forward Limit", self._right_faults.ForwardLimitSwitch
+        )
+        SmartDashboard.putBoolean(
+            "L Reverse Limit", self._left_climber.isRevLimitSwitchClosed()
+        )
+        SmartDashboard.putBoolean(
+            "R Reverse Limit", self._right_climber.isRevLimitSwitchClosed()
+        )
 
 
 class MoveClimber(Command):
